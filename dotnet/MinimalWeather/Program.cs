b300fed8729c757eb232f06b10f34a7407dcf688
Stephen Halter <halter73@gmail.com>
2021-04-28 16:06:10 -0700

    Add weather types

--- dotnet/MinimalWeather/Program.cs
+++ dotnet/MinimalWeather/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using MinimalWeather;
 
 var app = WebApplication.Create(args);
 
@@ -17,38 +19,9 @@ using var httpClient = new HttpClient()
 var baseQueryString = $"api-version=1.0&subscription-key={app.Configuration["SubscriptionKey"]}";
 
 app.MapGet("/weather/current/{location}", (Coordinate location) =>
-    httpClient.GetStringAsync($"currentConditions/json?{baseQueryString}&unit=imperial&query={location}"));
+     httpClient.GetFromJsonAsync<WeatherResults>($"currentConditions/json?{baseQueryString}&unit=imperial&query={location}"));
+
+app.MapGet("/proxyweather/current/{location}", (Coordinate location) =>
+     httpClient.GetStringAsync($"currentConditions/json?{baseQueryString}&unit=imperial&query={location}"));
 
 app.Run();
-
-public record Coordinate(double Latitude, double Longitude)
-{
-    public static bool TryParse(string input, out Coordinate coordinate)
-    {
-        coordinate = default;
-        var splitArray = input.Split(',', 2);
-
-        if (splitArray.Length != 2)
-        {
-            return false;
-        }
-
-        if (!double.TryParse(splitArray[0], out var lat))
-        {
-            return false;
-        }
-
-        if (!double.TryParse(splitArray[1], out var lon))
-        {
-            return false;
-        }
-
-        coordinate = new Coordinate(lat, lon);
-        return true;
-    }
-
-    public override string ToString()
-    {
-        return $"{Latitude},{Longitude}";
-    }
-}
