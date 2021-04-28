ca12e69963db711e7d472efd7511fc5a820799ca
Stephen Halter <halter73@gmail.com>
2021-04-28 15:41:54 -0700

    Add "/weather/current/{location}"

--- dotnet/MinimalWeather/Program.cs
+++ dotnet/MinimalWeather/Program.cs
@@ -1,14 +1,54 @@
 using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
-await using var app = WebApplication.Create(args);
+var app = WebApplication.Create(args);
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
+using var httpClient = new HttpClient()
+{
+    BaseAddress = new Uri("https://atlas.microsoft.com/weather/")
+};
+var baseQueryString = $"api-version=1.0&subscription-key={app.Configuration["SubscriptionKey"]}";
 
-await app.RunAsync();
+app.MapGet("/weather/current/{location}", (Coordinate location) =>
+    httpClient.GetStringAsync($"currentConditions/json?{baseQueryString}&unit=imperial&query={location}"));
+
+app.Run();
+
+public record Coordinate(double Latitude, double Longitude)
+{
+    public static bool TryParse(string input, out Coordinate coordinate)
+    {
+        coordinate = default;
+        var splitArray = input.Split(',', 2);
+
+        if (splitArray.Length != 2)
+        {
+            return false;
+        }
+
+        if (!double.TryParse(splitArray[0], out var lat))
+        {
+            return false;
+        }
+
+        if (!double.TryParse(splitArray[1], out var lon))
+        {
+            return false;
+        }
+
+        coordinate = new Coordinate(lat, lon);
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return $"{Latitude},{Longitude}";
+    }
+}
