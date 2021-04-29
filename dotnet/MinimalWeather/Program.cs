ab10625fb2df3e194fdbd8118e40e7daf9bd2333
Stephen Halter <halter73@gmail.com>
2021-04-28 17:27:07 -0700

    Add forecast support

--- dotnet/MinimalWeather/Program.cs
+++ dotnet/MinimalWeather/Program.cs
@@ -12,16 +12,29 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+var baseQueryString = $"api-version=1.0&subscription-key={app.Configuration["SubscriptionKey"]}&unit=imperial";
 using var httpClient = new HttpClient()
 {
     BaseAddress = new Uri("https://atlas.microsoft.com/weather/")
 };
-var baseQueryString = $"api-version=1.0&subscription-key={app.Configuration["SubscriptionKey"]}";
 
-app.MapGet("/weather/current/{location}", (Coordinate location) =>
-     httpClient.GetFromJsonAsync<WeatherResults>($"currentConditions/json?{baseQueryString}&unit=imperial&query={location}"));
+app.MapGet("/weather/{location}/current", (Coordinate location) =>
+     httpClient.GetFromJsonAsync<CurrentWeather>($"currentConditions/json?{baseQueryString}&query={location}"));
 
-app.MapGet("/proxyweather/current/{location}", (Coordinate location) =>
-     httpClient.GetStringAsync($"currentConditions/json?{baseQueryString}&unit=imperial&query={location}"));
+app.MapGet("/weather/{location}/forecast/hourly", (Coordinate location) =>
+     httpClient.GetFromJsonAsync<HourlyForecast>($"forecast/hourly/json?{baseQueryString}&query={location}&duration=24"));
+
+app.MapGet("/weather/{location}/forecast/daily", (Coordinate location) =>
+     httpClient.GetFromJsonAsync<DailyForecast>($"forecast/daily/json?{baseQueryString}&query={location}&duration=10"));
+
+// These endpoints are temporary to give UI devs a chance to grab data we're not forwarding yet.
+app.MapGet("/proxyweather/{location}/current", (Coordinate location) =>
+     httpClient.GetStringAsync($"currentConditions/json?{baseQueryString}&query={location}"));
+
+app.MapGet("/proxyweather/{location}/forecast/hourly", (Coordinate location) =>
+     httpClient.GetStringAsync($"forecast/hourly/json?{baseQueryString}&query={location}&duration=24"));
+
+app.MapGet("/proxyweather/{location}/forecast/daily", (Coordinate location) =>
+     httpClient.GetStringAsync($"forecast/daily/json?{baseQueryString}&query={location}&duration=10"));
 
 app.Run();
---
+++ dotnet/MinimalWeather/WeatherModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace MinimalWeather
+{
+    public class WeatherSnapshot
+    {
+        public DateTimeOffset DateTime { get; set; }
+        public string Phrase { get; set; }
+        public Temperature Temperature { get; set; }
+
+        public DateTimeOffset Date
+        {
+            set { DateTime = value; }
+        }
+
+        public string IconPhrase
+        {
+            set { Phrase = value; }
+        }
+    }
+
+    public class Temperature
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+    }
+
+    public class MinMaxTemperature
+    {
+        public Temperature Minimum { get; set; }
+        public Temperature Maximum { get; set; }
+    }
+
+    public class PhraseOnly
+    {
+        public string Phrase { get; set; }
+
+        public string IconPhrase
+        {
+            set { Phrase = value; }
+        }
+    }
+
+    public class FullDayForecast
+    {
+        public DateTimeOffset DateTime { get; set; }
+        public MinMaxTemperature Temperature { get; set; }
+
+        public PhraseOnly Day { get; set; }
+        public PhraseOnly Night { get; set;  }
+
+        public DateTimeOffset Date
+        {
+            set { DateTime = value; }
+        }
+    }
+
+    public class CurrentWeather
+    {
+        public WeatherSnapshot[] Results { get; set; }
+    }
+
+    public class HourlyForecast
+    {
+        public WeatherSnapshot[] Forecasts { get; set; }
+    }
+
+    public class DailyForecast
+    {
+        public FullDayForecast[] Forecasts { get; set; }
+    }
+}
