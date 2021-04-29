f9ea053ec1a3a6d6a63ae9699ba101bd702fe98f
Stephen Halter <halter73@gmail.com>
2021-04-29 14:38:45 -0700

    Remove extra files

--- dotnet/MinimalWeather/WeatherModel.cs
+++ dotnet/MinimalWeather/WeatherModel.cs
@@ -69,4 +69,36 @@ namespace MinimalWeather
     {
         public FullDayForecast[] Forecasts { get; set; }
     }
+
+    public record Coordinate(double Latitude, double Longitude)
+    {
+        public static bool TryParse(string input, out Coordinate coordinate)
+        {
+            coordinate = default;
+            var splitArray = input.Split(',', 2);
+
+            if (splitArray.Length != 2)
+            {
+                return false;
+            }
+
+            if (!double.TryParse(splitArray[0], out var lat))
+            {
+                return false;
+            }
+
+            if (!double.TryParse(splitArray[1], out var lon))
+            {
+                return false;
+            }
+
+            coordinate = new Coordinate(lat, lon);
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Latitude},{Longitude}";
+        }
+    }
 }
