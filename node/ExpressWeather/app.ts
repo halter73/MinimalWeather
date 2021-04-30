731bc8ab5f30662d021b2ea549b0ec1f714a3877
Stephen Halter <halter73@gmail.com>
2021-04-30 16:13:53 -0700

    Add '/weather/:lat,:lon' route to ExpressWeather

--- node/ExpressWeather/app.ts
+++ node/ExpressWeather/app.ts
@@ -1,9 +1,40 @@
 import * as express from 'express';
+import got from 'got';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var baseUrl = "https://atlas.microsoft.com/weather/";
+var baseQuery = `api-version=1.0&subscription-key=${process.env["SubscriptionKey"]}&unit=imperial`;
+
+app.get('/weather/:lat,:lon', async (req, res, next) => {
+    try {
+        const lat = parseFloat(req.params.lat);
+        const lon = parseFloat(req.params.lon);
+
+        const currentQuery = got(`${baseUrl}currentConditions/json?${baseQuery}&query=${lat},${lon}`);
+        const hourlyQuery = got(`${baseUrl}forecast/hourly/json?${baseQuery}&query=${lat},${lon}&duration=24`);
+        const dailyQuery = got(`${baseUrl}forecast/daily/json?${baseQuery}&query=${lat},${lon}&duration=10`);
+
+        const [currentResponse, hourlyResponse, dailyResponse] = await Promise.all([currentQuery, hourlyQuery, dailyQuery]);
+
+        const currentWeather = JSON.parse(currentResponse.body);
+        const hourlyForecast = JSON.parse(hourlyResponse.body);
+        const dailyForecast = JSON.parse(dailyResponse.body);
+
+        await res.json({
+            currentWeather: currentWeather.results[0],
+            hourlyForcasts: hourlyForecast.forecasts,
+            dailyForecasts: dailyForecast.forecasts,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500);
+    res.send();
 });
 
 const port = process.env.PORT || 3000
