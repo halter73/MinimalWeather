2e53bda1d3a514409fa05ba05169d697b9b2d5f9
Stephen Halter <halter73@gmail.com>
2021-04-30 13:35:43 -0700

    [EnableCors("weather")]

--- dotnet/MinimalWeather/Program.cs
+++ dotnet/MinimalWeather/Program.cs
@@ -2,10 +2,20 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MinimalWeather;
 
-var app  = WebApplication.Create(args);
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("weather", policyBuilder =>
+    {
+        policyBuilder.AllowAnyOrigin();
+    });
+});
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
@@ -18,7 +28,9 @@ using var httpClient = new HttpClient()
     BaseAddress = new Uri("https://atlas.microsoft.com/weather/")
 };
 
-app.MapGet("/weather/{location}", async (Coordinate location) =>
+app.UseCors();
+
+app.MapGet("/weather/{location}", [EnableCors("weather")] async (Coordinate location) =>
 {
     var currentQuery = httpClient.GetFromJsonAsync<CurrentWeather>($"currentConditions/json?{baseQueryString}&query={location}");
     var hourlyQuery = httpClient.GetFromJsonAsync<HourlyForecast>($"forecast/hourly/json?{baseQueryString}&query={location}&duration=24");
