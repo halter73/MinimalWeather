f2e823555b532bf30c9512a819875340a2f4b523
Stephen Halter <halter73@gmail.com>
2021-04-30 14:50:21 -0700

    Add ExpressWeather

--- node/ExpressWeather/app.ts
+++ node/ExpressWeather/app.ts
@@ -3,22 +3,8 @@ import * as express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    throw new Error();
     res.send('Hello World!');
 });
-
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err[ 'status' ] = 404;
-    next(err);
-});
-
-if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
-        res.status(err[ 'status' ] || 500);
-        res.send();
-    });
-}
 
 const port = process.env.PORT || 3000
 
